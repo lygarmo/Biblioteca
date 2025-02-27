f86dabfc3ebe9f75eab142813d833f92827f906b
Lydia <124252166+lygarmo@users.noreply.github.com>
2025-02-27 16:52:00 +0100

    Update Conexion.php

--- parte1/Conexion.php
+++ parte1/Conexion.php
@@ -1,1 +1,24 @@
+<?php
+    class Conexion{
+        private $host="localhost";
+        private $db="biblioteca";
+        private $user="root";
+        private $password="root";
+        private $conexion;
 
+        public function conexion(){
+            $this->conexion=null;
+
+            try{
+                $this->conexion=new PDO("mysql:host=".$this->host.";dbname=".$this->db, $this->user, $this->password);
+                $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e){
+                echo "Error: " . $e->getMessage();
+            }
+
+            return $this->conexion;
+        }
+
+    }
+
+?>
